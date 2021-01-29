f818dc4ccacbfb7be0ba8d2f5d6e838f26d514ed
agarcia-imt <agarcia.imt@gmail.com>
2021-01-28 18:57:14 -0600

    Navigation 3 Home Scrollview

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,13 +1,45 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { ScrollView, View, Text, Button } from 'react-native';
+import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
+const RenderItem = ({ item }) => {
+  // const { item } = props;
+  if (item != null) {
+    return (
+      <Card>
+        <Card.Image style={{ alignItems: 'center', justifyContent: 'center' }} source={require('./images/uthappizza.png')}>
+          <Card.FeaturedTitle>{item.name}</Card.FeaturedTitle>
+          <Card.FeaturedSubtitle>{item.designation}</Card.FeaturedSubtitle>
+        </Card.Image>
+        <Text style={{ margin:10 }}>{item.description}</Text>
+      </Card>
+    );
+  } else  {
+    return (<View></View>);
+  }
+}
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
+    }
+  }
   render() {
+    const { dishes, promotions, leaders } = this.state;
     return (
-      <View>
-        <Text>Home Component</Text>
+      <ScrollView>
+        <RenderItem item={ dishes.filter(dish => dish.featured)[0] } />
+        <RenderItem item={ promotions.filter(promo => promo.featured)[0] } />
+        <RenderItem item={ leaders.filter(leader => leader.featured)[0] } />
         <Button title='Drawer' onPress={ () => this.props.navigation.openDrawer()}/>
-      </View>
+      </ScrollView>
     )
   }
 }
