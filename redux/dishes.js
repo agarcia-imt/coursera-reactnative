597a508ba02a0b90fb61c59a38128b62f942e51e
agarcia-imt <agarcia.imt@gmail.com>
2021-02-03 16:05:37 -0600

    redux setup 1

---
+++ redux/dishes.js
@@ -0,0 +1,23 @@
+import * as ActionTypes from './ActionTypes';
+
+export const dishes = (state = {
+  isLoading: true,
+  errMess: null,
+  dishes: []
+}, action) => {
+  switch(action.type) {
+    case ActionTypes.ADD_DISHES:
+      return {...state, isLoading: false, errMess: null, dishes: action.payload};
+      break;
+    case ActionTypes.DISHES_LOADING:
+      return {...state, isLoading: true, errMess: null, dishes: []};
+      break;
+    case ActionTypes.DISHES_FAILED:
+      return {...state, isLoading: false, errMess: action.payload, dishes: []};
+      break;
+    default:
+      return state;
+  }
+};
+
+
